63aad5e1f4adb4fe9e88fa419e7289be829c7455
Patrick <ptrckwlkr@gmail.com>
2018-10-25 18:18:50 -0400

    Added get_entities() and get_map()

--- include/level.h
+++ include/level.h
@@ -10,9 +10,11 @@ class Level
 public:
     Level() : map(nullptr) {};
     std::shared_ptr<Map> get_map() {return map;}
+    std::vector<std::shared_ptr<Entity>> get_entities() {entities;}
 
 protected:
     std::shared_ptr<Map> map;
+    std::vector<std::shared_ptr<Entity>> entities;
 };
 
 
