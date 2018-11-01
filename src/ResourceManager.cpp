8111d43cc3eebeddf880404f375cdc2f62a57533
olivialeon <livleon1@gmail.com>
2018-11-01 18:24:11 -0400

    resourcemanager

---
+++ src/ResourceManager.cpp
@@ -0,0 +1,30 @@
+#ifndef CSCI437_RESOURCEMANAGER_H
+#define CSCI437_RESOURCEMANAGER_H
+
+#include <map> 
+#include <SFML/Graphics.hpp> 
+
+class ResourceManager{
+    public:  
+        ResourceManager(){}
+        ~ResourceManager(){}
+
+        void LoadTexture( std::string name, std::string fileName );
+        sf::Texture &GetTexture( std:: string name);
+
+        void LoadFont( std::string name, std::string fileName );
+        sf::Font &GetFont( std:: string name);
+
+        void LoadSprite( std::string name, std::string filename);
+        sf::Sprite &GetSprite( std:: string name);
+
+
+    private:
+        std::map<std::string, sf::Texture> _textures;
+        std::map<std::string, sf::Font> _fonts;
+        std::map<std::string, sf::Sprite> _sprites;
+
+};
+
+
+#endif //CSCI437_RESOURCEMANAGER_H
