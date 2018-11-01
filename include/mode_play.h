8b2361528e9a0adc41211be5346a2c08be1ed6a8
Rane Squires <pixel_dragon@yahoo.com>
2018-11-01 13:53:20 -0400

    Removed add mode_menu.h file

--- include/mode_play.h
+++ include/mode_play.h
@@ -1,21 +1,24 @@
-#ifndef CSCI437_MODE_MENU_H
-#define CSCI437_MODE_MENU_H
+#ifndef CSCI437_MODE_PLAY_H
+#define CSCI437_MODE_PLAY_H
 
 #include "mode.h"
 #include "level.h"
 #include "level_factory.h"
 
-class MenuMode : public Mode
+class PlayMode : public Mode
 {
 
 public:
-    MenuMode();
-    ~MenuMode();
+    PlayMode();
+    ~PlayMode();
     void update() override;
+    std::shared_ptr<Level> get_level() {return level;}
 
 private:
+    LevelFactory *level_factory;
+    std::shared_ptr<Level> level;
 
 };
 
 
-#endif //CSCI437_MODE_MENU_H
+#endif //CSCI437_MODE_PLAY_H
