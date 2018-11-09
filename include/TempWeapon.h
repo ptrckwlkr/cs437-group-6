d4ee77deb1327dcd96361316ccd2770d75f9dc2e
Patrick <ptrckwlkr@gmail.com>
2018-11-09 17:47:02 -0500

    Fixed weird error with conflicting file names

---
+++ include/TempWeapon.h
@@ -0,0 +1,28 @@
+#ifndef CSCI437_WEAPON_H
+#define CSCI437_WEAPON_H
+
+#include "entity.h"
+class TempWeapon
+{
+
+    TempWeapon();
+    ~TempWeapon();
+
+private: 
+    int maxRange;
+    int maxDamage;
+
+public: 
+    void set_position(Position new_pos);
+
+    void set_maxDamage( int maxD);
+        
+    void set_maxRange( int maxR);
+
+    void HandleEvents( Event* event);
+
+    void attack(Direction dir, float delta);
+
+};
+
+#endif //CSCI437_WEAPON_H
