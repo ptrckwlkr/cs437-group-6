99d4613195eac7bc85a3cad4c7ac3c20570e0903
olivialeon <livleon1@gmail.com>
2018-11-11 01:44:36 -0500

    w

---
+++ include/Weapon.h
@@ -0,0 +1,27 @@
+#ifndef CSCI437_WEAPON_H
+#define CSCI437_WEAPON_H
+
+#include "entity.h"
+class Weapon
+{
+
+    Weapon();
+    ~Weapon();
+
+private: 
+    int maxRange;
+    int maxDamage;
+
+public:
+
+    void set_maxDamage( int maxD);
+        
+    void set_maxRange( int maxR);
+
+    void HandleEvents( Event* event);
+
+    void attack(Direction dir, float delta);
+
+};
+
+#endif //CSCI437_WEAPON_H
