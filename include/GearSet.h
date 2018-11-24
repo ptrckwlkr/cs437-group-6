db7c3d141bda4da2cd163cc456ef033468c83d1e
Rane Squires <pixel_dragon@yahoo.com>
2018-11-24 06:55:24 -0500

    Added new classes for equipment items and a container for equipped items ("gear set"); more functionality to come

---
+++ include/GearSet.h
@@ -0,0 +1,22 @@
+#ifndef CSCI431_GEARSET_H
+#define CSCI431_GEARSET_H
+
+#include "Equipment.h"
+#include <unordered_map>
+#include <utility>
+#include <string>
+
+class GearSet
+{
+	private:
+		std::unordered_map<std::string, Equipment*> equippedItems;
+
+	protected:
+
+	public:
+		GearSet();
+		void equipItem(Equipment*);
+		Equipment* getItem(EquipSlot, int = 1);
+};
+
+#endif //CSCI431_GEARSET_H
