1afbc03f0d77adb7f945a342a999446b4ab0c907
Patrick <ptrckwlkr@gmail.com>
2018-11-28 17:17:33 -0500

    Random cleanup, enemies only chase Player if they are within a certain distance

--- include/event.h
+++ include/event.h
@@ -1,26 +1,13 @@
 #ifndef CSCI437_EVENT_H
 #define CSCI437_EVENT_H
 
-#include <string>
-
 typedef long long EventType;
-
-using std::string;
-
-enum EVENTID
-{
-    EVENT_COLLISION,
-    EVENT_GOLD_COLLECTION,
-    EVENT_PLAYER_SHOOT_AT,
-    EVENT_ENEMY_SHOT_AT,
-};
 
 class Event {
 
 public:
     Event() = default;
     ~Event() = default;
-    inline EVENTID EventId() const {return EVENT_ENEMY_SHOT_AT;}
     virtual const EventType& getEventType() const = 0;
 
 };
--- src/views/view_skeleton.cpp
+++ src/views/view_skeleton.cpp
@@ -1,3 +1,4 @@
+#include "macros.h"
 #include "views/view_skeleton.h"
 
 SkeletonView::SkeletonView(GameLogic *state, Skeleton &s) : View(state)
@@ -9,7 +10,7 @@ void SkeletonView::update(float delta)
 {
   Vector2D dir = state->get_level().get_player()->get_position() - skeleton->get_position();
   float hypo = state->get_level().get_player()->get_size() + skeleton->get_size();
-  if (hypo * hypo < dir.x * dir.x + dir.y * dir.y)
+  if (dir.length < ENEMY_ACTIVE_DISTANCE && hypo * hypo < dir.length * dir.length)
   {
     skeleton->move(dir, delta);
   }
