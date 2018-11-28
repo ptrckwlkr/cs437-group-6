ffe3c6d4e050bc46e2da341b8ac45446b7a7465c
iwscivera <iwright@scivera.com>
2018-11-28 14:57:15 -0500

    added delta to player attack

--- include/Player.h
+++ include/Player.h
@@ -7,22 +7,29 @@
 #include "Projectile.h"
 #include "listener.h"
 
-class Player: public Entity
-{
+class Player : public Entity {
 
-    private: 
-        int health = 20;
-        int mana;
-        //std::shared_ptr<PlayerData> PlayerData;
+private:
+    int health = 20;
+    int mana;
+    //std::shared_ptr<PlayerData> PlayerData;
 
 public:
-		Player(float x, float y);
+    Player(float x, float y);
 
-		~Player();
-		void update(double delta) {};
-		void attack(Vector2D &dir);
-		void HandleEvent(Event* event) override;
+    ~Player();
 
-}; 
+    void update(double delta) {};
+
+    void attack(Vector2D &dir, float delta);
+
+    void HandleEvent(Event *event) override;
+
+    //used to prevent more than one projectile firing each frame (assuming 60fps)
+    //NOTE THIS CAN BE CHANGED TO SIMULATE DIFFERENT WEAPON SPEED
+    float delta_threshold = 2 / 60.0;
+    float delta_sum;
+
+};
 
 #endif //CSCI437_PLAYER_H
--- src/Player.cpp
+++ src/Player.cpp
@@ -2,34 +2,37 @@
 #include <view_projectile.h>
 #include "macros.h"
 
-Player::Player(float x, float y) : Entity(x, y, 10)
-{
-  speed = PLAYER_SPEED;
-  type = TYPE_PLAYER;
-  EventManager::Instance()->RegisterObject(EVENT_GOLD_COLLECTION, this);
-  //EventManager::Instance()->RegisterObject(EVENT_PLAYER_PROJECTILE_COLLISION, this);
+Player::Player(float x, float y) : Entity(x, y, 10) {
+    speed = PLAYER_SPEED;
+    type = TYPE_PLAYER;
+    EventManager::Instance()->RegisterObject(EVENT_GOLD_COLLECTION, this);
+    //EventManager::Instance()->RegisterObject(EVENT_PLAYER_PROJECTILE_COLLISION, this);
+    delta_sum = 0.0;
 }
 
 
-Player::~Player()
-{
+Player::~Player() {
     //unsubscirbe from events 
 }
 
-void Player::HandleEvent(Event* event)
-{
-  // if (event->EventId() == EVENT_GOLD_COLLECTION) printf("PLAYER: I have collected gold!\n"); // TODO
-  // if (event->EventId()==EVENT_PLAYER_PROJECTILE_COLLISION){
-  //   health -= (int)(event->Arg1());
-  //   printf("PLAYER: I have been shoot at \n");
-  // }
+void Player::HandleEvent(Event *event) {
+    // if (event->EventId() == EVENT_GOLD_COLLECTION) printf("PLAYER: I have collected gold!\n"); // TODO
+    // if (event->EventId()==EVENT_PLAYER_PROJECTILE_COLLISION){
+    //   health -= (int)(event->Arg1());
+    //   printf("PLAYER: I have been shoot at \n");
+    // }
 }
 
-void Player::attack(Vector2D &dir)
-{
-  float x = EntityManager::Instance()->getPlayer()->get_position().x;
-  float y = EntityManager::Instance()->getPlayer()->get_position().y;
-  auto projectile = EntityManager::Instance()->createEntity<Projectile>(x, y);
-  projectile->set_direction(dir);
-  ViewManager::Instance()->add_view<Projectile, ProjectileView>(projectile);
+void Player::attack(Vector2D &dir, float delta) {
+    delta_sum += delta;
+    //prevents projectiles from being spammed
+    if (delta_sum >= delta_threshold) {
+        float x = EntityManager::Instance()->getPlayer()->get_position().x;
+        float y = EntityManager::Instance()->getPlayer()->get_position().y;
+        auto projectile = EntityManager::Instance()->createEntity<Projectile>(x, y);
+        projectile->set_direction(dir);
+        ViewManager::Instance()->add_view<Projectile, ProjectileView>(projectile);
+        delta_sum = 0;
+    }
+
 }
