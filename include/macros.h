0654f558cadf8dc65742a15fb40c88c9fd1e9cc7
Patrick <ptrckwlkr@gmail.com>
2018-11-29 00:49:57 -0500

    "Play music" is now a macro, to improve load speed during development

--- include/macros.h
+++ include/macros.h
@@ -7,6 +7,7 @@
 #define GAME_CLOCK_SCALER           2.f
 #define ZOOM_SCALAR					        1.0f
 #define NUMBER_OF_LEVELS            7
+#define MUSIC                       false
 
 #define PLAYER_SPEED				        100
 #define CELL_SIZE                   64      //HAS TO BE A MULTIPLE OF TILE MAP TEXTURE SIZE!
