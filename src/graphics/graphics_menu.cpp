d901d0d3c1768bac8c5496b46e8056476052a963
iwscivera <iwright@scivera.com>
2018-12-04 12:53:15 -0500

    fixed bug with about screen not working, no idea why a certain if statement was duplicated and it didn't seem to do anything so it was deleted

--- src/graphics/graphics_menu.cpp
+++ src/graphics/graphics_menu.cpp
@@ -32,8 +32,6 @@ void MenuGraphics::draw(sf::RenderTarget &target, sf::RenderStates states) const
 			target.draw(controls[i], states);
 	}
 	else if (view->screenIndex == 2)	//draw elements for about screen
-		target.draw(storyText, states);
-	else if (view->screenIndex == 3)	//draw elements for about screen
 		target.draw(aboutText, states);
 
 	//always draws the menu instructions so the player is not confused about how to operate the menu
