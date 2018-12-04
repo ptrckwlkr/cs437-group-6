de02f6c50a69de3bc27af84ec06cd9cb9c5f9ea5
Patrick <ptrckwlkr@gmail.com>
2018-12-03 22:03:15 -0500

    Ghosts die in a puff of smoke

--- src/entities/entity_types.cpp
+++ src/entities/entity_types.cpp
@@ -7,6 +7,7 @@
 #include "entities/Player.h"
 #include "entities/Projectile.h"
 #include "entities/skeleton.h"
+#include "entities/smoke.h"
 #include "entities/ghost.h"
 #include "entities/sword.h"
 
@@ -18,5 +19,6 @@ const EntityType Money::entityType        = (EntityType)(&Money::entityType);
 const EntityType Player::entityType       = (EntityType)(&Player::entityType);
 const EntityType Projectile::entityType   = (EntityType)(&Projectile::entityType);
 const EntityType Skeleton::entityType     = (EntityType)(&Skeleton::entityType);
+const EntityType Smoke::entityType        = (EntityType)(&Smoke::entityType);
 const EntityType Ghost::entityType        = (EntityType)(&Ghost::entityType);
 const EntityType Sword::entityType        = (EntityType)(&Sword::entityType);
